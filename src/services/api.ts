7ce0c61f8d8bfafc9793872ae4cf2c529eddde09
Isai Reyes <anonymus1201a@gmail.com>
2026-06-24 16:58:13 -0600

    Actualizacion de API

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://v3.football.api-sports.io",
   headers: {
-    "x-rapidapi-key": "TU_API_KEY",
+    "x-rapidapi-key": "11161a3b4e577021db00da9e9fa55f59",
     "x-rapidapi-host": "v3.football.api-sports.io",
   },
 });
