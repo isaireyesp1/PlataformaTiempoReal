e05b71c547af0efe51a3beb57c11df111f65b4cb
Isai Reyes <anonymus1201a@gmail.com>
2026-06-27 19:11:02 -0600

    Actualizacion de Tabla Eliminatorias

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,9 +1,11 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
 
 import HomeScreen from "../screens/HomeScreen";
 import MatchesScreen from "../screens/MatchesScreen";
 import StandingsScreen from "../screens/StandingsScreen";
+import KnockoutScreen from "../screens/KnockoutScreen";
 
 const Tab = createBottomTabNavigator();
 
@@ -11,9 +13,46 @@ export default function AppNavigator() {
   return (
     <NavigationContainer>
       <Tab.Navigator
-        screenOptions={{
+        screenOptions={({ route }) => ({
           headerShown: false,
-        }}
+
+          tabBarActiveTintColor: "#0A84FF",
+          tabBarInactiveTintColor: "#8E8E93",
+
+          tabBarStyle: {
+            height: 65,
+            paddingBottom: 8,
+            paddingTop: 6,
+            backgroundColor: "#FFFFFF",
+            borderTopWidth: 0,
+            elevation: 10,
+          },
+
+          tabBarIcon: ({ color, size }) => {
+            let iconName: keyof typeof Ionicons.glyphMap =
+              "home-outline";
+
+            if (route.name === "Inicio")
+              iconName = "home-outline";
+
+            if (route.name === "Partidos")
+              iconName = "football-outline";
+
+            if (route.name === "Grupos")
+              iconName = "grid-outline";
+
+            if (route.name === "Eliminatorias")
+              iconName = "trophy-outline";
+
+            return (
+              <Ionicons
+                name={iconName}
+                size={size}
+                color={color}
+              />
+            );
+          },
+        })}
       >
         <Tab.Screen
           name="Inicio"
@@ -26,8 +65,13 @@ export default function AppNavigator() {
         />
 
         <Tab.Screen
-          name="Tabla"
+          name="Grupos"
           component={StandingsScreen}
+        />
+
+        <Tab.Screen
+          name="Eliminatorias"
+          component={KnockoutScreen}
         />
       </Tab.Navigator>
     </NavigationContainer>
